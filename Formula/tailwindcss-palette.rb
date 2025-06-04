656ddf4f02d6379a6fa6b5077476dce249cf4398
Clayton Chew <30117799+claytonchew@users.noreply.github.com>
2025-06-04 17:09:05 +0800

    chore: publish tailwindcss-palette v0.1.0

---
+++ Formula/tailwindcss-palette.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+VERSION = '0.1.0'
+class TailwindcssPalette < Formula
+  desc "Generate Tailwind CSS color palettes from a base hex color"
+  homepage "https://github.com/claytonchew/tailwindcss-palette-go"
+  url "https://github.com/claytonchew/tailwindcss-palette-go.git", tag: "v#{VERSION}"
+  version VERSION
+  license "MIT"
+  head "https://github.com/claytonchew/tailwindcss-palette-go.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build",
+           "-ldflags",
+           "-s -w -X github.com/claytonchew/tailwindcss-palette-go/internal/version.Version=#{version} -X github.com/claytonchew/tailwindcss-palette-go/internal/version.CommitHash=#{Utils.git_short_head || "unknown"} -X github.com/claytonchew/tailwindcss-palette-go/internal/version.BuildDate=#{Time.now.utc.iso8601}",
+           "-o", "tailwindcss-palette",
+           "./cmd/tailwindcss-palette"
+    bin.install "tailwindcss-palette"
+  end
+
+  test do
+    system "#{bin}/tailwindcss-palette", "-v"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-tap
+
+Homebrew Formulae to some of my projects.
+
+**tailwindcss-palette**
+
+```
+brew install claytonchew/tap/tailwindcss-palette
+```
